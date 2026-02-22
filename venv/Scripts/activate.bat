b943bbaa37771db0a3c23333f1c0f070ad10e3c7
FathimaMehrinVS <mehrinfiza57@gmail.com>
2026-02-22 16:24:46 +0530

    Fixed dependency conflicts and clean setup

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hp\OneDrive\Desktop\My_Projects\FixTheGap\venv
+set VIRTUAL_ENV=C:\FixTheGap\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
